93457a4e9a87142ca16a75b06bdd4963d0c25648
Natali586 <8703518@mail.ru>
2021-08-18 18:18:54 +0300

    ЗаSelect  для  формирования первичного  отчета

---
+++ Превичный отчет запрос.sql
@@ -0,0 +1,3 @@
+ SELECT num_dog AS Номер_договора, cl_name AS ФИО, summa_dog as Сумма_договора,
+ date_begin as Дата_начала, date_end as Дата_окончания, d_summa as Сумма_кредита, d_procent as Проценты,
+ date_check as Дата_расчета, status as Статус  from final_report;
